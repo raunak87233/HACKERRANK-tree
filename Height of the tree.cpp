8ccd7c638c2ac138c596a807ba4c3cd6fb5a5db6
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2023-01-19 16:48:54 +0530

    Create Height of the tree.cpp

---
+++ Height of the tree.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+          int leftHeight=-1,rightHeight=-1;            //cause height is starting from 0
+    if(root->left){
+        leftHeight=height(root->left);
+    }
+    if(root->right){
+        rightHeight=height(root->right);
+    }
+    return max(leftHeight,rightHeight)+1;
+}
+}; //End of Solution
